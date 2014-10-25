d9d1e8131d0db715522b82af5318bed9ae68acee
Stefan Johnson <ScrimpyCat@gmail.com>
2014-10-26 00:46:43 +1100

    Use items from database

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,11 +1,13 @@
 class PageController < ApplicationController
     def home
+    	@items = Item.all
     end
 
     def about
     end
 
     def order
+    	@items = Item.all
     end
 
     def contact
