9bebf767772cbe6bd636332be8f67db899d8d9fd
Stefan Johnson <ScrimpyCat@gmail.com>
2014-10-25 18:46:27 +1100

    Removed old project files

--- app/views/layouts/nav_main.blade.php
+++
@@ -1,9 +0,0 @@
-<nav id="pageNav" class="cf">
-	<ul>
-      <li><a href="/home" title="Home">Home</a></li>
-      <li><a href="/about" title="about cupcraves">About</a></li>
-      <li><a href="_source/menu.htm" title="cupcake menu">The cupcakes</a></li>
-      <li><a href="_source/specialty.htm" title="specialty upcakes">The specials</a></li>
-      <li><a href="_source/catalog.htm" title="cupcraves catalog">The catalog</a></li>
-	</ul>
-</nav>
