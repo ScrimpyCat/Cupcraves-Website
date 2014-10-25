bb27e419472b2e623c3c0fb926b4642b5b6f9b52
Stefan Johnson <ScrimpyCat@gmail.com>
2014-10-25 19:00:46 +1100

    Fix shrink when coming from homepage link

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,25 +1,26 @@
 Menu = {
-    expand: function(a){
+    expand: function(item){
         Menu.shrink();
-        if (Menu.current_item == a) Menu.current_item = null;
+
+        if (item.is(Menu.current_item)) Menu.current_item = null;
         else
         {
-            Menu.current_item = a;
-            $(a).parent().children(".menu-item-description").show();
+            Menu.current_item = item;
+            item.parent().children(".menu-item-description").show();
         }
     },
     shrink: function(){
-        if ((Menu.current_item != undefined) || (Menu.current_item != null)) $(Menu.current_item).parent().children(".menu-item-description").hide();
+        if ((Menu.current_item != undefined) || (Menu.current_item != null)) Menu.current_item.parent().children(".menu-item-description").hide();
     }
 };
 
 $(window).bind('page:change', function(){
     $(".menu-item").click(function(){
-        Menu.expand(this);
+        Menu.expand($(this));
     });
 
     if (window.location.hash)
     {
-        Menu.expand(window.location.hash + " .menu-item");
+        Menu.expand($(window.location.hash + " .menu-item"));
     }
 });
